de7c526698637c4c8885996c06fd4c59e6065d34
Your Namesanyamjain04 <sanyamjainsj04@gmail.com>
2022-11-08 13:10:19 +0530

    Added show playlist in home page by genre

---
+++ data/index.js
@@ -0,0 +1,17 @@
+import popTracks from "./popTracks.json";
+import hipPopTracks from "./hipPopTracks.json";
+import electronicTracks from "./electronicTracks.json";
+import rockTracks from "./rockTracks.json";
+import danceTracks from "./danceTracks.json";
+import musicTracksData from "./musicTracks.json";
+import houseTracks from "./houseTracks.json";
+
+export {
+  popTracks,
+  hipPopTracks,
+  electronicTracks,
+  rockTracks,
+  danceTracks,
+  musicTracksData,
+  houseTracks,
+};
